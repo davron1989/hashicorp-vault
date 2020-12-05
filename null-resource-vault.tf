884f25944866df5ce17e959c87bf53d63fe68f2e
davron1989 <davron.n@yahoo.com>
2020-12-05 02:37:50 +0000

    added docker-compose command

--- null-resource-vault.tf
+++ null-resource-vault.tf
@@ -15,9 +15,30 @@ resource "null_resource" "vault" {
     }
 
     inline = [
-      "sudo yum install httpd -y",
-      "sudo systemctl start httpd",
-      "sudo yum install httpd -y"
+      "yum install wget -y",
+      "wget -qO- https://get.docker.com/  | sh",
+      "sudo systemctl start docker",
+      "sudo systemctl enable docker",
+      "sudo yum install python3 python3-pip -y",
+      "sudo pip3 install docker-compose",
+      "mkdir -p "$PWD"/vault/{config,file.logs}"
+    ]
+  },
+
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = "centos"
+      private_key = "${file("~/.ssh/id_rsa")}"
+      host        = "${aws_instance.vault_server.public_ip}"
+    }
+    source = /vault/config/vault.json
+    destination = ./vault/config/vault.json
+    source = ./docker-compose.yml
+    destination =/~
+
+    inline = [
+        "docker-compose up -d"
     ]
   }
 }
