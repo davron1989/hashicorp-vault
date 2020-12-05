dbaa91639ed164b45f7e5b53011342f088f450ba
davron1989 <davron.n@yahoo.com>
2020-12-05 04:10:30 +0000

    added su root

--- null-resource-vault.tf
+++ null-resource-vault.tf
@@ -1,11 +1,10 @@
 resource "null_resource" "vault" {
-    triggers = {
-       always_run = "${timestamp()}"
-    }
-  
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
   depends_on = ["aws_instance.vault_server"]
 
-       
   provisioner "remote-exec" {
     connection {
       type        = "ssh"
@@ -15,29 +14,43 @@ resource "null_resource" "vault" {
     }
 
     inline = [
+      "sudo su root",
       "sudo yum install wget -y",
       "sudo wget -qO- https://get.docker.com/  | sh",
       "sudo systemctl start docker",
       "sudo systemctl enable docker",
       "sudo yum install python3 python3-pip -y",
       "sudo pip3 install docker-compose",
-      "mkdir -p vault/{config,file.logs}",
+      "mkdir -p vault/{config,file,logs}",
       "cd vault",
-#      "docker-compose up -d"
     ]
-  },
+  }
 
-    provisioner "file" {
+  provisioner "file" {
     connection {
       type        = "ssh"
       user        = "centos"
       private_key = "${file("~/.ssh/id_rsa")}"
       host        = "${aws_instance.vault_server.public_ip}"
     }
-    source = "./vault/volumes/config/vault.json"
-    destination = "~/vault/volumes/config/vault.json"
-    // source = "./vault/docker-compose.yml"
-    // destination = "~/vault"
+
+    source      = "./vault/volumes/config/vault.json"
+    destination = "~/vault/config/vault.json"
+    source      = "./vault/docker-compose.yml"
+    destination = "~/vault/docker-compose.yml"
   }
 
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = "centos"
+      private_key = "${file("~/.ssh/id_rsa")}"
+      host        = "${aws_instance.vault_server.public_ip}"
+    }
+
+    inline = [
+      "cd vault",
+      "docker-compose up -d",
+    ]
+  }
 }
